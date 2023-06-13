f2a55fa6c627d954f26334eb3996b8b62c1a119a
Hubert <burzynskihubert@gmail.com>
2023-06-13 14:50:22 +0200

    create model for Urls

--- main/tinyurl/models.py
+++ main/tinyurl/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import hashlib
 
 # Create your models here.
+class Urls(models.Model):
+    main_url = models.URLField()
+    tiny_url = models.CharField(max_length=10, unique=True, blank=True)
+    
+    def save(self):
+        if not self.tiny_url:
+            hash_object = hashlib.sha256(self.my_field.encode('utf-8'))
+            tiny_hash = hash_object.hexdigest()[:10]
+            self.tiny_hash = tiny_hash
+        super().save()
